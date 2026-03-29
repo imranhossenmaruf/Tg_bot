d1aa98d1c6d1b20852b0de16be059220a842ab6f
Imran Hossain Maruf <iimranhossenmaruf@gmail.com>
2026-03-29 23:07:16 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "your_bot_token"
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-MONGO_URI = "your_mongo_uri"
-ADMIN_IDS = [123456789]  # List of admin user IDs
-CHANNEL_IDS = ["channel_id_1", "channel_id_2"]  # List of required channel IDs
+BOT_TOKEN = "8689611964:AAGEmYwvIvEUzRF7O2nXDeO64KIup7fZTdo"
+API_ID = "26898464"
+API_HASH = "f7a42ef9f0e5c77ece5a9c8c163e1408"
+MONGO_URI = "mongodb+srv://talesbymaruf_db_user:C4skVgtLNf4av9Rd@cluster0.635zt2s.mongodb.net/?appName=Cluster0"
+ADMIN_IDS = [6770328841]  # List of admin user IDs
+CHANNEL_IDS = ["-1003744642897", "-1002314377422"]  # List of required channel IDs
