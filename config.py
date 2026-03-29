76c76532cbe2bd579907d0c97056290a7a9d6009
Imran Hossain Maruf <iimranhossenmaruf@gmail.com>
2026-03-29 23:02:53 +0600

    Remove commented header from config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "your_bot_token"
+API_ID = "your_api_id"
+API_HASH = "your_api_hash"
+MONGO_URI = "your_mongo_uri"
+ADMIN_IDS = [123456789]  # List of admin user IDs
+CHANNEL_IDS = ["channel_id_1", "channel_id_2"]  # List of required channel IDs
